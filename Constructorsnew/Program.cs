bd0b99b0c5b25e7f9ee32069842c5a94a3b91f2c
kumar6646 <sai.penusoft@gmail.com>
2025-07-07 12:35:04 +0530

    Made changes...

--- Constructorsnew/Program.cs
+++ Constructorsnew/Program.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace Constructorsnew
 {
+    //wap to create a class student with sid, sname, sage as data members.
+    //different students can have different sid, sname, sage.
+    //Parameterized constructor should be used to initialize the data members.
+    /*
+     * While defining a constructor, we can use the same name as the class name.
+     * while defining a constructor if we have used parametes is called as
+     * parameterized constructor.
+     * A class can contain multiple parameterized constructors.
+     * but we should differentiate with the number of parameters and the type of parameters and
+     * order of parameters or signatures.
+     * what is signature will represent?
+     * signature will represent the number of parameters and the type of parameters and order of 
+     * parameters.
+     * 
+     * Constrcutor Overloading is a concept where we can have multiple constructors in a class
+     * Defing multiple constructors in a class is called as constructor overloading.
+     * 
+     */
     class Employee
     {
         int eid;
